6645503591d6fa4f3664583a78ccdd91c52b9f94
Vit <slonik0775@gmail.com>
2022-04-05 01:57:40 +0300

    commit project

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
 
+
+
+
 'use strict';
 
 
@@ -12,13 +15,32 @@ const personalMovieDB = {
     privat: false,
 };
 
-const lastFilms = prompt('Один из последних просмотренных фильмов?', '');
-const lastFilms2 = prompt('Один из последних просмотренных фильмов?', '');
-const filmScore = prompt('На сколько оцените его?', '');
-const filmScore2 = prompt('На сколько оцените его?', '');
 
 
-personalMovieDB.movies[lastFilms] = filmScore;
-personalMovieDB.movies[lastFilms2] = filmScore2;
+for(let i = 0; i < 2; i++){
+    const a  = prompt('Один из последних просмотренных фильмов?', '');
+    const b = prompt('На сколько оцените его?', '');
+
+    if(a != null && b != null && a != '' && b != '' && a.length <50){
+  
+personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
+
+if(personalMovieDB.count < 10){
+
+console.log('Просмотрено довольно мало фильмов');
+}else if(personalMovieDB.count < 30 && personalMovieDB.count > 10){
+    console.log('Вы классический зритель');
+
+}else if(personalMovieDB.count >= 30){
+    console.log('Вы киноман');
+
+}else {
+    console.log('Произошла ошибка');
+}
+
 
 console.log(personalMovieDB);
